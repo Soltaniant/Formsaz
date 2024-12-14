5106e613a83e2813c01f44d30326b16160fc4ec7
s.soltanian <s.soltaniant@gmail.com>
2024-12-14 20:02:18 +0330

    init project with spring initializer

---
+++ src/main/java/com/example/formsaz/FormsazApplication.java
@@ -0,0 +1,13 @@
+package com.example.formsaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formsaz/FormsazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formsaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
