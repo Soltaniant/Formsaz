d3dd0551bfe20878f395a664788d65aab7ceb810
s.soltanian <s.soltaniant@gmail.com>
2024-12-28 21:00:51 +0330

    add controller

---
+++ src/main/java/com/example/formsaz/controller/FormController.java
@@ -0,0 +1,67 @@
+package com.example.formsaz.controller;
+
+import com.example.formsaz.model.dto.*;
+import com.example.formsaz.serivce.FormService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/forms")
+public class FormController {
+
+    private final FormService formService;
+
+    public FormController(FormService formService) {
+        this.formService = formService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FormResponseDto>> getAllForms() {
+        return ResponseEntity.ok(formService.getAllForms());
+    }
+
+    @PostMapping
+    public ResponseEntity<FormResponseDto> createForm(@RequestBody CreateFormRequestDto formDto) {
+        return ResponseEntity.ok(formService.createForm(formDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FormResponseDto> getFormById(@PathVariable Long id) {
+        return ResponseEntity.ok(formService.getFormById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FormResponseDto> updateForm(@PathVariable Long id,
+                                                      @RequestBody UpdateFormRequestDto updatedFormDto) {
+        return ResponseEntity.ok(formService.updateForm(id, updatedFormDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteForm(@PathVariable Long id) {
+        formService.deleteForm(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/fields")
+    public ResponseEntity<List<FieldResponseDto>> getFieldsByFormId(@PathVariable Long id) {
+        return ResponseEntity.ok(formService.getFieldsByFormId(id));
+    }
+
+    @PutMapping("/{id}/fields")
+    public ResponseEntity<List<FieldResponseDto>> updateFieldsByFormId(@PathVariable Long id,
+                                                                       @RequestBody List<FieldRequestDto> fieldDtos) {
+        return ResponseEntity.ok(formService.updateFieldsByFormId(id, fieldDtos));
+    }
+
+    @PutMapping("/{id}/publish")
+    public ResponseEntity<FormResponseDto> togglePublishForm(@PathVariable Long id) {
+        return ResponseEntity.ok(formService.togglePublishForm(id));
+    }
+
+    @GetMapping("/published")
+    public ResponseEntity<List<FormResponseDto>> getPublishedForms() {
+        return ResponseEntity.ok(formService.getPublishedForms());
+    }
+}
